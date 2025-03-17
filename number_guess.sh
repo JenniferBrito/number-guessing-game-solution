c8cf856b6c4b9c525bea4f5a71e5360c4f652221
Jennifer Brito <jennifer.brito3@gmail.com>
2025-03-17 19:12:09 +0000

    fix: minor code adjustments

--- number_guess.sh
+++ number_guess.sh
@@ -13,49 +13,50 @@ MAIN_GAME(){
   read USERNAME
 
   # checking if username is registred
-  PLAYER_USERNAME=$($PSQL "SELECT username FROM player WHERE username = '$USERNAME'";) 
+  #PLAYER_USERNAME=$($PSQL "SELECT username FROM player WHERE username = '$USERNAME'";) 
   PLAYER_ID=$($PSQL "SELECT player_id FROM player WHERE username = '$USERNAME';")
-  if  [[ -z $PLAYER_USERNAME ]]
+  if  [[ -z $PLAYER_ID ]]
   then
     INSERT_PLAYER=$($PSQL "INSERT INTO player(username) VALUES('$USERNAME');") 
     echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   else
   # get player_id
-    GAMES_PLAYED=$($PSQL "SELECT COUNT(player_id) FROM games WHERE player_id = $PLAYER_ID;")
-    BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games WHERE player_id = $PLAYER_ID;")
-    echo -e "\nWelcome back, $PLAYER_USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+    GAMES_PLAYED=$($PSQL "SELECT COUNT(player_id) FROM games LEFT JOIN player USING(player_id) WHERE player_id = $PLAYER_ID;")
+    BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games LEFT JOIN player USING(player_id) WHERE player_id = $PLAYER_ID;")
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
 
   DIV=$((1000+1))
-  R=$(($RANDOM%$DIV))
+  SECRET_NUMBER=$(($RANDOM%$DIV))
   
   echo -e "\nGuess the secret number between 1 and 1000:"
   read GUESS
-  declare -i GUESS_COUNT=0
-  while [[ $GUESS -ne $R ]]
+  declare -i NUMBER_OF_GUESSES=0
+  while [[ $GUESS -ne $SECRET_NUMBER ]]
   do
     if [[ ! $GUESS =~ ^[0-9]+$ ]]
     then
       echo -e "\nThat is not an integer, guess again:"
       read GUESS
-      ((GUESS_COUNT++))
+      ((NUMBER_OF_GUESSES++))
     fi
-    if [[ $GUESS -lt $R ]]
+    if [[ $GUESS -lt $SECRET_NUMBER ]]
     then
       echo -e "\nIt's higher than that, guess again:"
       read GUESS
-      ((GUESS_COUNT++))
-    elif [[ $GUESS -gt $R ]]
+      ((NUMBER_OF_GUESSES++))
+    elif [[ $GUESS -gt $SECRET_NUMBER ]]
     then 
         echo -e "\nIt's lower than that, guess again:"
         read GUESS
-        ((GUESS_COUNT++))
+        ((NUMBER_OF_GUESSES++))
     fi
   done
-  echo -e "\nYou guessed it in $GUESS_COUNT tries. The secret number was $R. Nice job!"
-
-  INSERT_GAME=$($PSQL "INSERT INTO games(number_of_guesses, player_id) VALUES($GUESS_COUNT, $PLAYER_ID);")
-  echo "$INSERT_GAME"
+  echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+  
+  PLAYER_ID=$($PSQL "SELECT player_id FROM player WHERE username = '$USERNAME';")
+  INSERT_GAME=$($PSQL "INSERT INTO games(number_of_guesses, player_id) VALUES($NUMBER_OF_GUESSES, $PLAYER_ID);")
+  #echo "$INSERT_GAME"
   
 }
 
